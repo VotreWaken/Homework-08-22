fab62a2d7beb0a9eb3a45d10a1a13af1fca6ee81
Waken <votrevincentwaken@gmail.com>
2023-09-12 16:48:46 +0300

    Add project files.

---
+++ ConsoleApp38/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        List<object> collection = new List<object>
+        {
+            42, "Hello, World!", DateTime.Now, 3.14
+        };
+
+            Thread displayThread = new Thread(() => DisplayElements(collection));
+            displayThread.Start();
+
+            displayThread.Join();
+
+            Console.WriteLine("Done");
+        }
+
+        static void DisplayElements(List<object> collection)
+        {
+            foreach (var element in collection)
+            {
+                string result = element.ToString();
+                Console.WriteLine("Result: " + result);
+            }
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+class Bank
+{
+    private int money;
+    private string name;
+    private int percent;
+    private object lockObject = new object();
+    private Thread writerThread;
+
+    public int Money
+    {
+        get => money;
+        set
+        {
+            lock (lockObject)
+            {
+                money = value;
+                StartWriterThread();
+            }
+        }
+    }
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            lock (lockObject)
+            {
+                name = value;
+                StartWriterThread();
+            }
+        }
+    }
+
+    public int Percent
+    {
+        get => percent;
+        set
+        {
+            lock (lockObject)
+            {
+                percent = value;
+                StartWriterThread();
+            }
+        }
+    }
+
+    public Bank()
+    {
+        writerThread = new Thread(WritePropertiesToFile);
+        writerThread.Start();
+    }
+
+    private void WritePropertiesToFile()
+    {
+        while (true)
+        {
+            lock (lockObject)
+            {
+                using (StreamWriter writer = new StreamWriter("bank_properties.txt"))
+                {
+                    writer.WriteLine($"Money: {money}");
+                    writer.WriteLine($"Name: {name}");
+                    writer.WriteLine($"Percent: {percent}");
+                }
+            }
+            Thread.Sleep(1000);
+        }
+    }
+
+    private void StartWriterThread()
+    {
+        if (!writerThread.IsAlive)
+        {
+            writerThread = new Thread(WritePropertiesToFile);
+            writerThread.Start();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bank bank = new Bank();
+
+        bank.Money = 10000;
+        bank.Name = "My Bank";
+        bank.Percent = 5;
+
+
+        bank.Money = 15000;
+
+        Console.WriteLine("Done");
+    }
+}
